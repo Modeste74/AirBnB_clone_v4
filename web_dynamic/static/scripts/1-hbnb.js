11e42ff59177753a7aa22a53ac57feb94e722a11
Modeste Ciira <ciiramodeste23@gmail.com>
2023-10-06 15:41:29 +0300

    Select some Amenities to be comfortable

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  const amenityIDs = {};
+  $('input[type="checkbox"]').change(function () {
+    const amenityID = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if (this.checked) {
+      amenityIDs[amenityID] = amenityName;
+    } else {
+      delete amenityIDs[amenityID];
+    }
+    const amenitiesList = Object.values(amenityIDs).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+});
